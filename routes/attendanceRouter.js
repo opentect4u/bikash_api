1efd6acb873edf4e87a4b714931d4869426c8ae7
Subham0529 <samantasubham9804@gmail.com>
2022-11-04 18:40:04 +0530

    attendance router 04-11-2022

--- routes/attendanceRouter.js
+++ routes/attendanceRouter.js
@@ -2,7 +2,7 @@ const express = require('express'),
     attRouter = express.Router(),
     dateFormat = require('dateformat');
 
-const { F_Select } = require('../models/MasterModule');
+const { F_Select, F_Insert } = require('../models/MasterModule');
 
 attRouter.get('/atten', async (req, res) => {
     var data = req.query;
@@ -16,12 +16,13 @@ attRouter.get('/atten', async (req, res) => {
 })
 
 attRouter.post('/atten', async (req, res) => {
-    var data = req.query;
-    var datetime = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss');
+    var data = req.body;
+    var datetime = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
+        date = dateFormat(new Date(), 'yyyy-mm-dd');
     var table_name = 'td_attendance',
         fields = data.sl_no > 0 ? `check_out = "${data.check_out}", modified_by = "${data.user}", modified_dt = "${datetime}"` :
-            `(ardb_id, user_id, emp_code, check_in, lat_pos, long_pos, created_by, created_dt)`,
-        values = `("${data.ardb_id}", "${data.user_id}", "${data.emp_code}", "${data.check_in}", "${data.lat_pos}", "${data.long_pos}", "${data.user}", "${datetime}")`,
+            `(ardb_id, user_id, emp_code, atten_date, check_in, lat_pos, long_pos, loc_name, created_by, created_dt)`,
+        values = `("${data.ardb_id}", "${data.user_id}", "${data.emp_code}", "${date}", "${data.check_in}", "${data.lat_pos}", "${data.long_pos}", "${data.loc_name}", "${data.user}", "${datetime}")`,
         whr = data.sl_no > 0 ? `sl_no = ${data.sl_no}` : null,
         flag = data.sl_no > 0 ? 1 : 0;
     var res_dt = await F_Insert(table_name, fields, values, whr, flag)
--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -80,4 +80,37 @@ reportRouter.post('/tour_prog', async (req, res) => {
     })
 })
 
+reportRouter.get('/attendance', async (req, res) => {
+    var ardb_id = req.session.user.ardb_id;
+    var select = 'user_name, user_id',
+        table_name = 'md_user',
+        whr = `ardb_id = "${ardb_id}" AND user_type = 'U' AND user_status = 'A'`,
+        order = null;
+    var res_dt = await F_Select(select, table_name, whr, order)
+    // console.log(res_dt);
+    res.render('report/atten_in', {
+        heading: "Attendance Report",
+        emp_list: res_dt.suc > 0 ? res_dt.msg : null
+    })
+})
+
+reportRouter.post('/attendance', async (req, res) => {
+    var data = req.body,
+        ardb_id = req.session.user.ardb_id,
+        frm_dt = data.frm_dt,
+        to_dt = data.to_dt,
+        emp_name = data.emp_name;
+    // console.log();
+    var select = '*',
+        table_name = 'td_attendance',
+        whr = data.id > 0 ? `sl_no = "${data.id}"` : `ardb_id = "${ardb_id}" AND user_id = "${data.emp_id}" AND atten_date BETWEEN "${frm_dt}" AND "${to_dt}"`,
+        order = null;
+    var res_dt = await F_Select(select, table_name, whr, order)
+    res.render('report/atten_out', {
+        heading: "Attendance Report",
+        prog_dt: res_dt.suc > 0 ? res_dt.msg : null,
+        frm_dt, to_dt, emp_name, dateFormat
+    })
+})
+
 module.exports = { reportRouter }
